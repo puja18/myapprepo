1f3fa2ebddb33e26c57d5d255fb2102ebf14d018
DIN77003032$ <DIN77003032$@DIN77003032.corp.capgemini.com>
2021-03-08 15:44:47 +0530

    first commit

---
+++ appdemo/src/main/java/org/cap/demo/appdemo/main.java
@@ -0,0 +1,13 @@
+package org.cap.demo.appdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
